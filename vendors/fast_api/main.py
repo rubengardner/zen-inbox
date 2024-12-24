02164dcb59af38272280bc67bd6d0060dc50e9db
rubengardner <rgardnercuesta@gmail.com>
2024-12-24 13:32:48 +0100

    Add CORS fast api setup

--- vendors/fast_api/main.py
+++ vendors/fast_api/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from auth.api.auth_api import auth_router
 from emails.api.email_api import email_router
 
+allowed_origins = ["http://localhost:3000"]
 app = FastAPI()
 
 
 app.include_router(auth_router)
 
 app.include_router(email_router)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
